56e50d28cd3c7d908a0bdc3a68818499048ebb5f
diwakar4586 <diwakar0470.be21@chitkara.edu.in>
2024-04-28 20:34:03 +0530

    added few screens

---
+++ SportX/HomeViewController.swift
@@ -0,0 +1,31 @@
+//
+//  HomeViewController.swift
+//  SportX
+//
+//  Created by student on 28/04/24.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func unwindToStartingViewController(_ unwindSegue: UIStoryboardSegue) {
+        // You can perform any additional actions here if needed
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SportX/SelectSportViewController.swift
@@ -0,0 +1,34 @@
+//
+//  SelectSportViewController.swift
+//  SportX
+//
+//  Created by student on 28/04/24.
+//
+
+import UIKit
+
+class SelectSportViewController: UIViewController {
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+//
+    }
+    @IBAction func buttonPressed(_ sender: UIButton) {
+//        performSegue(withIdentifier: "showHomeScreen", sender: nil)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
