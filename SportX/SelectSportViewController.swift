344d823f0637d6877150f7906502c8c759f0feca
Diwakar <diwakar0470.be21@chitkara.edu.in>
2024-05-12 23:59:59 +0530

    Revert "Daksh"

--- SportX/SelectSportViewController.swift
+++ SportX/SelectSportViewController.swift
@@ -12,16 +12,23 @@ class SelectSportViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // Do any additional setup after loading the view.
         
+//
     }
     @IBAction func buttonPressed(_ sender: UIButton) {
-        let destinationVC = self.storyboard?.instantiateViewController(identifier: "MainTabBarController")
-        
-        if let destinationVC {
-            self.navigationController?.pushViewController(destinationVC, animated: true)
-            print(self.navigationController)
-            // present(destinationVC, animated: true)
-        }
+//        performSegue(withIdentifier: "showHomeScreen", sender: nil)
     }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
